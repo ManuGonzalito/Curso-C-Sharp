c4b8438baba7c753f964547cde8578803de0eec4
Manuel González <manu.ale.gonzalez@gmail.com>
2024-09-10 16:58:36 -0300

    Ejercicios hasta la clase 62

---
+++ Genericos/Program.cs
@@ -0,0 +1,51 @@
+﻿
+AlmacenObjetos<string> archivos = new AlmacenObjetos<string>(4);
+archivos.Agregar("Juan");
+archivos.Agregar("Elena");
+archivos.Agregar("Antonio");
+archivos.Agregar("Sandra");
+
+string nombrePersona = (string)archivos.getElemento(2);
+Console.WriteLine(nombrePersona);
+
+// archivos.Agregar(new Empleado(1500));
+// archivos.Agregar(new Empleado(2500));
+// archivos.Agregar(new Empleado(3500));
+// archivos.Agregar(new Empleado(4500));
+
+// Empleado salarioEmpleado = (Empleado) archivos.getElemento(2);
+// Console.WriteLine(salarioEmpleado.getSalario());
+
+
+
+class AlmacenObjetos<T>
+{
+    private T[] datosElemento;
+    private int i = 0;
+    public AlmacenObjetos(int z)
+    {
+        datosElemento = new T[z];
+    }
+    public void Agregar(T obj)
+    {
+        datosElemento[i] = obj;
+        i++;
+    }
+    public T getElemento(int i)
+    {
+        return datosElemento[i];
+    }
+}
+
+class Empleado
+{
+    private double salario;
+    public Empleado(double salario)
+    {
+        this.salario = salario;
+    }
+    public double getSalario()
+    {
+        return salario;
+    }
+}
---
+++ GenericosRestricciones/Program.cs
@@ -0,0 +1,76 @@
+﻿AlmacenEmpleados<Director> empleados = new AlmacenEmpleados<Director>(3);
+empleados.Agregar(new Director(4500));
+empleados.Agregar(new Director(1500));
+empleados.Agregar(new Director(2500));
+
+class AlmacenEmpleados<T> where T: IParaEmpleados
+{
+    private int i = 0;
+    private T[] datosEmpleados;
+    public AlmacenEmpleados(int z)
+    {
+        datosEmpleados = new T[z];
+    }
+    public void Agregar(T obj)
+    {
+        datosEmpleados[i] = obj;
+        i++;
+    }
+    public T getEmpleado(int i)
+    {
+        return datosEmpleados[i];
+    }
+}    
+
+class Director : IParaEmpleados
+{
+    private double salario;
+    public Director(double salario)
+    {
+        this.salario = salario;
+    }
+    public double getSalario()
+    {
+        return salario;
+    }
+}
+class Secretaria : IParaEmpleados
+{
+    private double salario;
+    public Secretaria(double salario)
+    {
+        this.salario = salario;
+    }
+    public double getSalario()
+    {
+        return salario;
+    }
+}
+class Electricista : IParaEmpleados
+{
+    private double salario;
+    public Electricista(double salario)
+    {
+        this.salario = salario;
+    }
+    public double getSalario()
+    {
+        return salario;
+    }
+}
+class Estudiante
+{
+    private double edad;
+    public Estudiante(double edad)
+    {
+        this.edad = edad;
+    }
+    public double getEdad()
+    {
+        return edad;
+    }
+}
+interface IParaEmpleados
+{
+    double getSalario();
+}
